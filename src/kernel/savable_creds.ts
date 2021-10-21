af7f7dc9bfc25397582823ac195964535b27afc0
Joe Lapp <arachnojoe@gmail.com>
2021-10-21 08:57:54 -0500

    Commented SavableCredentials

--- src/kernel/savable_creds.ts
+++ src/kernel/savable_creds.ts
@@ -1,15 +1,33 @@
 import * as keytar from 'keytar'
 
+/**
+ * SavableCredentials is a class representing login credentials that can be
+ * saved in a desktop application. It can save at most one username/password
+ * pair. Whether it saves or clears saved credentials is configurable. The
+ * caller is responsible for saving the username, so that this class can
+ * ensure that a password is only ever returned for the expected username.
+ * Regardless of whether the class is saving credentials, it keeps an
+ * internal copy of the credentials for use by the application.
+ */
 export abstract class SavableCredentials {
 
   protected serviceName: string
   protected username?: string
   protected password?: string
 
+  /**
+   * Constructs an instance associating credentials with a service name.
+   * @param serviceName Name of the service to which the credentials apply.
+   *     Should be unique across all applications.
+   */
   constructor(serviceName: string) {
     this.serviceName = serviceName
   }
 
+  /**
+   * Initializes prior to use, either loading previously-saved credentials
+   * or clearing previously-saved credentials, as configured.
+   */
   async init(): Promise<void> {
     const savedUsername = this.getSavedUsername()
     if (this.isSavingCredentials() && savedUsername) {
@@ -25,6 +43,9 @@ export abstract class SavableCredentials {
       await this.clear() // to be safe; shouldn't be necessary
   }
 
+  /**
+   * Clears any previously-saved credentials associated with the service.
+   */
   async clear(): Promise<void> {
     const creds = await keytar.findCredentials(this.serviceName)
     for (const cred of creds)
@@ -33,18 +54,40 @@ export abstract class SavableCredentials {
     this.password = undefined
   }
 
+  /**
+   * Returns the current credentials as a [username, password] tuple, if
+   * assigned. Otherwise returns null.
+   */
   get(): [string, string] | null {
     if (this.username && this.password)
       return [this.username, this.password]
     return null
   }
 
+  /**
+   * Returns the saved username when configured to save credentials.
+   * Should return "" when there is no saved username.
+   */
   abstract getSavedUsername(): string
 
+  /**
+   * Indicates whether the application is presently configured to save
+   * the associated credentials. The return value may dynamically change
+   * over the course of the application.
+   */
   abstract isSavingCredentials(): boolean
 
+  /**
+   * Saves the indicated username for use retrieving its associated
+   * password in the future.
+   * @param username Username to save
+   */
   abstract saveUsername(username: string): Promise<void>
 
+  /**
+   * Sets the credentials. If configured to save credentials, also
+   * stores the credentials for future retrieval under this username.
+   */
   async set(username: string, password: string): Promise<void> {
     if (this.isSavingCredentials()) {
       await this.saveUsername(username)
