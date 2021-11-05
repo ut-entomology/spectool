6eee0ab416cff60cfd517c7457362fbb5dddb022
Joe Lapp <arachnojoe@gmail.com>
2021-11-05 09:17:51 -0500

    Made Modal independent of Bootstrap internals.

--- src/frontend/components/Modal.svelte
+++ src/frontend/components/Modal.svelte
@@ -3,48 +3,47 @@
     Displays a modal dialog using Bootstrap's modal javascript.
 
     Exports `showModal()` and `hideModal()` for showing the identified modal and
-    for hiding whichever modal is active. Do not use the `data-bs-*` attributes
-    with this component.
+    for hiding whichever modal is active. Do not use Bootstrap's `data-bs-*`
+    attributes with this component.
   */
 
-  const currentModalStore = writable<any>(null);
-  let currentModal: any = null; // a bootstrap modal object
+  const currentModalIDStore = writable<string | null>(null);
+  let currentModalID: string | null = null;
 
-  currentModalStore.subscribe((modal) => {
-    currentModal = modal;
+  currentModalIDStore.subscribe((modalID) => {
+    currentModalID = modalID;
   });
 
   export type ModalOptions = {
+    // defined by Bootstrap
     backdrop?: boolean | string;
     keyboard?: boolean;
     focus?: boolean;
   };
 
   export function showModal(id: string, options?: ModalOptions) {
-    if (currentModal !== null) throw Error('Modal already showing');
+    if (currentModalID !== null) throw Error('Modal already showing');
     options ||= {};
     if (options.backdrop === undefined) {
       options.backdrop = 'static';
     }
-
     const modal = new bootstrap.Modal(document.getElementById(id), options);
-    currentModalStore.set(modal);
+    currentModalIDStore.set(id);
     modal.show();
   }
 
   export function hideModal(afterHideCallback?: () => void) {
-    if (currentModal === null) throw Error('No modal to hide');
-    currentModal.hide();
+    if (currentModalID === null) throw Error('No modal to hide');
+    const modalElement = document.getElementById(currentModalID);
     if (afterHideCallback) {
-      const modalElement = document.getElementById(currentModal._element.id);
       modalElement!.addEventListener('hidden.bs.modal', function (_event) {
+        // Can't be sure the originally queued callback will run first.
+        currentModalIDStore.set(null);
+        // This callback can open another modal at this point, if desired.
         afterHideCallback();
       });
     }
-  }
-
-  function clearCurrentModal() {
-    currentModalStore.set(null);
+    bootstrap.Modal.getInstance(modalElement).hide();
   }
 </script>
 
@@ -59,22 +58,23 @@
   export let fade = false;
   export let maxWidth = '';
 
-  const modalClass = 'modal' + (fade ? ' fade' : '');
-  const innerStyle = maxWidth ? 'margin: 0 auto; max-width: ' + maxWidth : '';
-
   onMount(() => {
     // Only clear current modal when it is really hidden, and always do so.
     const modalElement = document.getElementById(id);
     modalElement!.addEventListener('hidden.bs.modal', function (_event) {
-      clearCurrentModal();
+      currentModalIDStore.set(null);
     });
   });
 </script>
 
-<div class={modalClass} {id} tabindex="-1" aria-hidden="true">
+<div {id} class={'modal' + (fade ? ' fade' : '')} tabindex="-1" aria-hidden="true">
   <div class={'modal-dialog ' + classNames}>
-    <div style={innerStyle}>
+    {#if maxWidth}
+      <div style={'margin: 0 auto; max-width: ' + maxWidth}>
+        <slot>No content provided</slot>
+      </div>
+    {:else}
       <slot>No content provided</slot>
-    </div>
+    {/if}
   </div>
 </div>
