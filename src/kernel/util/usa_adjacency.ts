60ba56abeaca25e5767069ce74496fbd2df45f51
Joe Lapp <arachnojoe@gmail.com>
2021-12-23 14:33:28 -0600

    Added states/provinces adjacent to USA

---
+++ src/kernel/util/usa_adjacency.ts
@@ -0,0 +1,129 @@
+// U.S. counties adjacent to Mexican states
+
+export const usaAdjacencies = {
+  AK: {
+    'North Slope Borough': ['Yukon'],
+    'Yukon-Koyukuk Census Area': ['Yukon'],
+    'Southeast Fairbanks Census Area': ['Yukon'],
+    'Valdez-Cordova Census Area': ['Yukon'],
+    'Yakutat City and Borough': ['Yukon', 'British Columbia'],
+    'Haines Borough': ['British Columbia'],
+    'Skagway Municipality': ['British Columbia'],
+    'Juneau City and Borough': ['British Columbia'],
+    'Hoonah-Angoon Census Area': ['British Columbia'],
+    'Petersburg Census Area': ['British Columbia'],
+    'Wrangell City and Borough': ['British Columbia'],
+    'Ketchikan Gateway Borough': ['British Columbia'],
+    'Prince of Wales-Hyder Census Area': ['British Columbia']
+  },
+  AZ: {
+    'Yuma County': ['Sonora'],
+    'Pima County': ['Sonora'],
+    'Santa Cruz County': ['Sonora'],
+    'Cochise County': ['Sonora']
+  },
+  CA: {
+    'San Diego County': ['Baja California'],
+    'Imperial County': ['Baja California']
+  },
+  ID: {
+    'Boundary County': ['British Columbia']
+  },
+  ME: {
+    'Oxford County': ['Quebec'],
+    'Franklin County': ['Quebec'],
+    'Somerset County': ['Quebec'],
+    'Aroostook County': ['Quebec', 'New Brunswick'],
+    'Washington County': ['New Brunswick', 'Nova Scotia']
+  },
+  MI: {
+    'Keweenaw County': ['Ontario'],
+    'Chippewa County': ['Ontario'],
+    'St. Clair County': ['Ontario'],
+    'Macomb County': ['Ontario'],
+    'Wayne County': ['Ontario'],
+    'Monroe County': ['Ontario']
+  },
+  MN: {
+    'Kittson County': ['Manitoba'],
+    'Roseau County': ['Manitoba'],
+    'Lake of the Woods County': ['Manitoba', 'Ontario'],
+    'Koochiching County': ['Ontario'],
+    'St. Louis County': ['Ontario'],
+    'Lake County': ['Ontario'],
+    'Cook County': ['Ontario']
+  },
+  MT: {
+    'Lincoln County': ['British Columbia'],
+    'Flathead County': ['British Columbia', 'Alberta'],
+    'Glacier County': ['British Columbia', 'Alberta'],
+    'Toole County': ['Alberta'],
+    'Liberty County': ['Alberta'],
+    'Hill County': ['Alberta', 'Saskatchewan'],
+    'Blaine County': ['Alberta', 'Saskatchewan'],
+    'Phillips County': ['Saskatchewan'],
+    'Valley County': ['Saskatchewan'],
+    'Daniels County': ['Saskatchewan'],
+    'Sheridan County': ['Saskatchewan']
+  },
+  NH: {
+    'Coos County': ['Quebec']
+  },
+  ND: {
+    'Divide County': ['Saskatchewan'],
+    'Burke County': ['Saskatchewan'],
+    'Renville County': ['Saskatchewan', 'Manitoba'],
+    'Bottineau County': ['Saskatchewan', 'Manitoba'],
+    'Rolette County': ['Manitoba'],
+    'Towner County': ['Manitoba'],
+    'Cavalier County': ['Manitoba'],
+    'Pembina County': ['Manitoba']
+  },
+  NM: {
+    'Hidalgo County': ['Sonora', 'Chihuahua'],
+    'Luna County': ['Chihuahua'],
+    'Doña Ana County': ['Chihuahua']
+  },
+  NY: {
+    'Erie County': ['Ontario'],
+    'Wyoming County': ['Ontario'],
+    'Jefferson County': ['Ontario'],
+    'St. Lawrence County': ['Ontario'],
+    'Franklin County': ['Ontario', 'Quebec'],
+    'Clinton County': ['Quebec']
+  },
+  TX: {
+    'El Paso County': ['Chihuahua'],
+    'Hudspeth County': ['Chihuahua'],
+    'Jeff Davis County': ['Chihuahua'],
+    'Presidio County': ['Chihuahua'],
+    'Brewster County': ['Chihuahua', 'Coahuila'],
+    'Terrell County': ['Coahuila'],
+    'Val Verde County': ['Coahuila'],
+    'Kinney County': ['Coahuila'],
+    'Maverick County': ['Coahuila'],
+    'Webb County': ['Coahuila', 'Nuevo León', 'Tamaulipas'],
+    'Zapata County': ['Tamaulipas'],
+    'Starr County': ['Tamaulipas'],
+    'Hidalgo County': ['Tamaulipas'],
+    'Cameron County': ['Tamaulipas']
+  },
+  VT: {
+    'Grand Isle County': ['Quebec'],
+    'Franklin County': ['Quebec'],
+    'Orleans County': ['Quebec'],
+    'Essex County': ['Quebec']
+  },
+  WA: {
+    'Clallam County': ['British Columbia'],
+    'Island County': ['British Columbia'],
+    'San Juan County': ['British Columbia'],
+    'Snohomish County': ['British Columbia'],
+    'Skagit County': ['British Columbia'],
+    'Whatcom County': ['British Columbia'],
+    'Okanogan County': ['British Columbia'],
+    'Ferry County': ['British Columbia'],
+    'Stevens County': ['British Columbia'],
+    'Pend Oreille County': ['British Columbia']
+  }
+};
