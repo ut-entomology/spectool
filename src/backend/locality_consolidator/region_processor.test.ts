d7c82cbcb86290df30680f364d7f34779db69493
Joe Lapp <arachnojoe@gmail.com>
2022-03-05 10:47:14 -0600

    Saving tweaks prior to revising for runProcessor()

--- src/backend/locality_consolidator/region_processor.test.ts
+++ src/backend/locality_consolidator/region_processor.test.ts
@@ -3,48 +3,50 @@ import type { LocalityCache } from './locality_cache';
 import type { TrackedRegion } from './tracked_region';
 import { RegionProcessor } from './region_processor';
 import { Region, RegionRank } from '../../shared/shared_geography';
-import { MockRegionAccess } from './mock/mock_region_access';
+import { MockRegionAccess, RegionNode } from './mock/mock_region_access';
 import { MockTrackedRegionRoster } from './mock/mock_tracked_region_roster';
 import { MockPhoneticCodeIndex } from './mock/mock_phonetic_code_index';
 import { MockPotentialSynonymsStore } from './mock/mock_potential_synonyms';
 import { MockExcludedMatchesStore } from './mock/mock_excluded_matches';
 import { PROCESS_SUBREGIONS_FLAG } from '../util/region_adjacencies';
-import type { LocalityMatch } from '../../shared/shared_locality';
+import type { LocalityMatch, LocalityData } from '../../shared/shared_locality';
 import { toPartialSortedPhoneticSeries } from './mock/phonetic_util';
 
+const localityDefaults = {
+  latitude: null,
+  longitude: null,
+  remarks: '',
+  lastModified: new Date('1-Jan-2022').getTime()
+};
+
 test('process isolated region, isolated locality', async () => {
-  const region1 = new TestRegion(1, 'Travis County', false);
-  const regionAccess = new MockRegionAccess(
-    {
-      region: region1,
-      localityCount: 2
-    },
-    {}
-  );
+  const regions = [new TestRegion(1, 'Travis County', false)];
+  const regionTree: RegionNode = {
+    region: regions[0],
+    localityCount: 2
+  };
+  const adjacencyMap: Record<number, Region[]> = {};
+  const localities = [
+    new CachedLocality(
+      Object.assign({}, localityDefaults, {
+        regionID: regions[0].id,
+        localityID: 10,
+        name: 'Zilker Preserve'
+      })
+    ),
+    new CachedLocality(
+      Object.assign({}, localityDefaults, {
+        regionID: regions[0].id,
+        localityID: 11,
+        name: 'Zilker Park'
+      })
+    )
+  ];
+
+  const regionAccess = new MockRegionAccess(regionTree, adjacencyMap);
   const phoneticCodeIndex = new MockPhoneticCodeIndex();
   const regionRoster = new MockTrackedRegionRoster();
-  regionRoster.getOrCreate(region1, true);
-  const trackedRegion1 = await regionRoster.getOrCreate(region1, true);
-  const localities = [
-    new CachedLocality({
-      regionID: trackedRegion1.id,
-      localityID: 10,
-      latitude: null,
-      longitude: null,
-      name: 'Zilker Preserve',
-      remarks: '',
-      lastModified: new Date('1-Jan-2022').getTime()
-    }),
-    new CachedLocality({
-      regionID: trackedRegion1.id,
-      localityID: 11,
-      latitude: null,
-      longitude: null,
-      name: 'Zilker Park',
-      remarks: '',
-      lastModified: new Date('1-Jan-2022').getTime()
-    })
-  ];
+  const trackedRegion1 = await regionRoster.getOrCreate(regions[0], true);
 
   const processor = new RegionProcessor(
     regionAccess,
@@ -157,4 +159,47 @@ function purgeCachedWordSeries(localityMatch: LocalityMatch): void {
       delete phoneticSubset.cachedWordSeries;
     }
   }
+}
+
+async function runProcessor(config: {
+  baselineDate: Date | null;
+  regionToProcess: Region;
+  domainRegions: Region[];
+  nondomainRegions: Region[];
+  regionTree: RegionNode;
+  adjacencyMap: Record<number, Region[]>;
+  localities: LocalityData[];
+}): Promise<LocalityMatch[]> {
+  const regionAccess = new MockRegionAccess(config.regionTree, config.adjacencyMap);
+  const phoneticCodeIndex = new MockPhoneticCodeIndex();
+  const regionRoster = new MockTrackedRegionRoster();
+
+  for (const region of config.domainRegions) {
+    await regionRoster.getOrCreate(region, true);
+  }
+  for (const region of config.nondomainRegions) {
+    await regionRoster.getOrCreate(region, false);
+  }
+
+  const processor = new RegionProcessor(
+    regionAccess,
+    new MockLocalityCache(
+      phoneticCodeIndex,
+      config.localities.map((data) => new CachedLocality(data))
+    ),
+    new MockPotentialSynonymsStore(),
+    phoneticCodeIndex,
+    regionRoster,
+    new MockExcludedMatchesStore()
+  );
+
+  const matches: LocalityMatch[] = [];
+  for await (const match of processor.run(
+    config.baselineDate,
+    (await regionRoster.getByID(config.regionToProcess.id))!
+  )) {
+    purgeCachedWordSeries(match);
+    matches.push(match);
+  }
+  return matches;
 }
