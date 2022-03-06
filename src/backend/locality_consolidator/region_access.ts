54be24de336fe44d5d5274b29c8d7f3a605a4d1e
Joe Lapp <arachnojoe@gmail.com>
2022-03-05 22:40:45 -0600

    Clarified meaning of localityCount

--- src/backend/locality_consolidator/region_access.ts
+++ src/backend/locality_consolidator/region_access.ts
@@ -7,5 +7,9 @@ export interface RegionAccess {
 
   getContainedRegions(underGeographyID: number): Region[];
 
+  /**
+   * Returns the number of localities indicating that the region of the provided
+   * ID is the most specific region to which the locality belongs.
+   */
   getLocalityCount(forSingleGeographicID: number): number;
 }
